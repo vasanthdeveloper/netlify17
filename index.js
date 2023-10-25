cd480cf1a991134a8ffad54d3083ff35c0a0602b
vasanthdeveloper <125169032+vasanthdeveloper@users.noreply.github.com>
2023-10-25 19:39:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById('counter').innerHTML=10;
+    setTimeout(() => {
+        document.getElementById('counter').innerHTML=9; 
+        setTimeout(()=>{
+            document.getElementById('counter').innerHTML=8; 
+            setTimeout(()=>{
+                document.getElementById('counter').innerHTML=7; 
+                setTimeout(()=>{
+                    document.getElementById('counter').innerHTML=6; 
+                    setTimeout(()=>{
+                        document.getElementById('counter').innerHTML=5; 
+                        setTimeout(()=>{
+                            document.getElementById('counter').innerHTML=4; 
+                            setTimeout(()=>{
+                                document.getElementById('counter').innerHTML=3; 
+                                setTimeout(()=>{
+                                    document.getElementById('counter').innerHTML=2; 
+                                    setTimeout(()=>{
+                                        document.getElementById('counter').innerHTML=1; 
+                                        setTimeout(()=>{
+                                            document.getElementById('counter').innerHTML='Happy Independence day'; 
+                                        },1000) 
+                                    },1000) 
+                                },1000) 
+                            },1000) 
+                        },1000) 
+                    },1000) 
+                },1000) 
+            },1000) 
+        },1000) 
+    },1000);
+},1000)
